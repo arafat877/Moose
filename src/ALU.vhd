7eb540cd4b41404a6f6bdac18ee2877472095543
Lazar Djurovic <djurovic.lazar@jjzmaj.edu.rs>
2017-12-23 17:40:52 +0100

    ALU v2
    Added zero and carry flag for operations

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -16,13 +16,21 @@ entity ALU is
     alu_input_a : in std_logic_vector (31 downto 0); -- first alu input
     alu_input_b : in std_logic_vector (31 downto 0); -- second alu input
     alu_command : in std_logic_vector (3 downto 0); -- command for alu
-    alu_output : out std_logic_vector ( 31 downto 0) -- output from the alu
-	
+    alu_output : out std_logic_vector ( 31 downto 0); -- output from the alu
+	 alu_flags : inout std_logic_vector ( 3 downto 0) --flags ( to store additional information about output of operation)
+		
+		-- flags(0) - zero flag
+		-- flags(1) - carry flag
+		-- flags(2) - signed flag
+		-- flags(3) - overflow flag
+		
 	);
   
 end ALU;
 
 architecture Behavioral of ALU is
+	
+	signal alu_output_resized : std_logic_vector (32 downto 0) := (others => '0');
 
 begin
 
@@ -42,9 +50,22 @@ begin
       case alu_command is
 
         when "0000" => alu_output <= (others => '0'); -- alu does nothing ( NOP )
-        when "0001" => alu_output <= std_logic_vector(signed(alu_input_a) + signed(alu_input_b)); -- a + b 
-		  when "0010" => alu_output <= std_logic_vector(signed(alu_input_a) - signed(alu_input_b)); -- a - b 
-        when "0011" => alu_output <= alu_input_a or alu_input_b; -- a or b
+        when "0001" => 
+			
+			alu_output <= std_logic_vector(signed(alu_input_a) + signed(alu_input_b)); -- a + b 
+			alu_output_resized <= std_logic_vector(signed('0' & alu_input_a) + signed('0' & alu_input_b)); -- first bit of this vector is carry flag
+			alu_flags(1) <= alu_output_resized(32); -- set carry flag to MSB of alu_output_resized			
+				
+		  when "0010" => 
+		  
+		  alu_output <= std_logic_vector(signed(alu_input_a) - signed(alu_input_b)); -- a - b 
+				if signed(alu_input_a) - signed(alu_input_b) = 0 then
+					alu_flags(0) <= '1';
+				else 
+					alu_flags(0) <= '0';
+				end if;
+			
+		  when "0011" => alu_output <= alu_input_a or alu_input_b; -- a or b
         when "0100" => alu_output <= alu_input_a and alu_input_b; -- a and b
         when "0101" => alu_output <= alu_input_a xor alu_input_b; -- a xor b
 		  when "0110" => alu_output <= not alu_input_a; -- invert a
