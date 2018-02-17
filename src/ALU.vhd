98370111f9c112d5e01969a9ade002a61372c515
lazardjurovic <lazar.lax4525@gmail.com>
2018-02-17 18:28:09 +0100

    Removed clock signal from ALU

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -11,12 +11,12 @@ use IEEE.NUMERIC_STD.ALL;
 entity ALU is
 
   port(
-
-    clk : in std_logic; -- clock input
+  
     alu_command : in std_logic_vector (3 downto 0); -- command for alu
-	 alu_flags : inout std_logic_vector ( 3 downto 0) := (others => '0'); --flags ( to store additional information about output of operation)
-	 alu_input : in std_logic_vector ( 31 downto 0) := (others => 'Z'); -- signal used for reading data from data bus
-	 alu_output : out std_logic_vector ( 31 downto 0) := (others => 'Z') -- signal used for writing to data bus
+	alu_flags : inout std_logic_vector ( 3 downto 0) := (others => '0'); --flags ( to store additional information about output of operation)
+	alu_input_a : in std_logic_vector (31 downto 0); -- first alu input
+    alu_input_b : in std_logic_vector (31 downto 0); -- second alu input
+	alu_output : out std_logic_vector ( 31 downto 0) := (others => 'Z') -- signal used for writing to data bus
 		
 		-- flags(0) - zero flag
 		-- flags(1) - carry flag
@@ -30,9 +30,7 @@ end ALU;
 architecture Behavioral of ALU is
 	
 	signal alu_output_resized : std_logic_vector (32 downto 0) := (others => '0');
-	signal alu_input_a : std_logic_vector (31 downto 0); -- first alu input
-   signal alu_input_b : std_logic_vector (31 downto 0); -- second alu input
-	signal alu_result :  std_logic_vector ( 31 downto 0); -- output from the alu
+
 
 begin
 
@@ -44,23 +42,21 @@ begin
 ----------
 --------------
 
-  alu_process : process(clk)
+  alu_process : process(alu_input_a,alu_input_b)
   begin
-
-    if rising_edge(clk) then
 
       case alu_command is
 
         when "0000" => alu_output <= (others => '0'); -- alu does nothing ( NOP )
         when "0001" => 
 			
-			alu_result <= std_logic_vector(signed(alu_input_a) + signed(alu_input_b)); -- a + b 
+			alu_output <= std_logic_vector(signed(alu_input_a) + signed(alu_input_b)); -- a + b 
 			alu_output_resized <= std_logic_vector(signed('0' & alu_input_a) + signed('0' & alu_input_b)); -- first bit of this vector is carry flag
 			alu_flags(1) <= alu_output_resized(32); -- set carry flag to MSB of alu_output_resized			
 				
 		  when "0010" => 
 		  
-		  alu_result <= std_logic_vector(signed(alu_input_a) - signed(alu_input_b)); -- a - b 
+		  alu_output <= std_logic_vector(signed(alu_input_a) - signed(alu_input_b)); -- a - b 
 		  
 				if alu_input_a = alu_input_b then
 					alu_flags(0) <= '1'; -- set zero flag
@@ -71,18 +67,14 @@ begin
 					alu_flags(2) <= '0';
 				end if;
 			
-		  when "0011" => alu_result <= alu_input_a or alu_input_b; -- a or b
-        when "0100" => alu_result <= alu_input_a and alu_input_b; -- a and b
-        when "0101" => alu_result <= alu_input_a xor alu_input_b; -- a xor b
-		  when "0110" => alu_result <= not alu_input_a; -- invert a
-		  when "0111" => alu_input_a <= alu_input; -- load first input parameter
-		  when "1000" => alu_input_b <= alu_input; -- load second parameter
-		  when "1001" => alu_output <= alu_result; --output result to the bus
-		  when others => alu_output <= (others => 'Z');	-- not a legal command
+		when "0011" => alu_output <= alu_input_a or alu_input_b; -- a or b
+        when "0100" => alu_output <= alu_input_a and alu_input_b; -- a and b
+        when "0101" => alu_output <= alu_input_a xor alu_input_b; -- a xor b
+		when "0110" => alu_output <= not alu_input_a; -- invert a
+		when others => alu_output <= (others => 'Z');	-- not a legal command
+		
       end case;
-      
-    end if;
-    
+       
   end process;
   
 
