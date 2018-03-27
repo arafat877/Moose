e6d0c63be133eabfc77d2d81335796aac5f88d02
bad63r <bad63r@gmail.com>
2018-03-27 22:14:06 +0200

    rewriting pipeline register

--- src/Pipeline_Register.vhd
+++ src/Pipeline_Register.vhd
@@ -1,28 +1,41 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity Pipeline_Register is
 	
 	port(
-		
-		data_in : in std_logic_vector( 31 downto 0);
-		enable : in std_logic;
+		data_in  : in std_logic_vector( 31 downto 0);
+		enable   : in std_logic;
 		data_out : out std_logic_vector( 31 downto 0)
-	
 	);
 	
 end Pipeline_Register;
 
 architecture Behavioral of Pipeline_Register is
 
-	signal internal_state : std_logic_vector ( 31 downto 0);
+  signal current_state, next_state : std_logic_vector( 31 downto 0);
+	signal internal_state            : std_logic_vector( 31 downto 0);
 
 begin
-	
-	internal_state <= data_in when enable = '1' else internal_state;
-	data_out <= internal_state;
+	process (clk, reset) is
+  begin  -- process
+    if reset = '1' then
+      current_state <= (others => '0');
+    elsif rising_edge(clk) then
+      current_state <= next_state;
+    end if;
+  end process;
 
+  process (clk, enable) is
+  begin  -- process
+    if enable = '1' then
+      next_state <= data_in;
+    else
+      next_state <= current_state;
+    end if;
+  end process;
+
+  data_out <= current_state;
 
 end Behavioral;
 
