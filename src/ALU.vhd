c3e1d38cb5fe23bd5f0dddc6f52ec68b622cc0f6
lazardjurovic <lazar.lax4525@gmail.com>
2018-06-28 10:07:22 +0200

    added I type instrustions

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -17,7 +17,7 @@ entity ALU is
         func7_2bit : in std_logic;
         rs1 : in std_logic_vector(31 downto 0);
         rs2 : in std_logic_vector(31 downto 0);
-        imm : in std_logic_vector(31 downto 0);
+        imm : in std_logic_vector(11 downto 0);
         inst_type : in std_logic; -- 1 for R type, 0 for I type
         res : out std_logic_vector(31 downto 0)
        
@@ -80,7 +80,43 @@ begin
         
    end if;
    
-   if inst_type = '0' then
+   if inst_type = '0' then -- I type instruction
+   
+        case func3 is
+        
+            when "000" =>  res <= std_logic_vector(unsigned(rs1)+unsigned(imm)); -- addi
+            
+            when "001" => res <= std_logic_vector(shift_left(unsigned(rs1),to_integer(unsigned(imm))));-- slli
+        
+             when "010" => --slt ( signed )
+                              
+                if signed(rs1) < signed(imm) then
+                    res <= "00000000000000000000000000000001";
+                else
+                    res <= (others => '0');
+                end if; 
+                                      
+             when "011" => -- sltu ( unsigned )
+                              
+                if unsigned(rs1) < unsigned(imm) then
+                    res <= "00000000000000000000000000000001";
+                else
+                    res <= (others => '0');
+                end if; 
+                      
+            when "100" => res <= rs1 xor imm;
+                      
+            when "101" =>
+                if imm(11) = '0' then -- srli
+                    res <= std_logic_vector(shift_right(unsigned(rs1),to_integer(unsigned(imm))));
+               else -- srai
+                    res <= std_logic_vector(shift_right(signed(rs1),to_integer(unsigned(imm))));
+               end if;
+                           
+           when "110" => res <= rs1 or imm; -- ori
+           when "111" => res <= rs1 and imm; -- andi
+        
+       end case;
    
    end if;
     
