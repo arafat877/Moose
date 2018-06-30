912608818a44d2f3ab498640530942d4f8ee269f
lazardjurovic <lazar.lax4525@gmail.com>
2018-06-30 12:28:33 +0200

    Updated code

--- src/Program_Counter.vhd
+++ src/Program_Counter.vhd
@@ -10,54 +10,27 @@ use ieee.numeric_std.all;
 
 entity Program_Counter is
 	
-	port(
-	
+	port(	
 		clk : in std_logic; -- clock signal
-		pc_in : in std_logic_vector( 31 downto 0) ; -- input for Program Counter ( connected to internal bus)
-		pc_out : out std_logic_vector( 31 downto 0) :=  (others => '0'); -- output of program counter ( connected to address bus)
-		pc_command : in std_logic_vector (1 downto 0) -- command for program counter
+		pc_input : in std_logic_vector( 31 downto 0);
+		pc_output : out std_logic_vector( 31 downto 0)
 	);
 
 end Program_Counter;
 
 architecture Behavioral of Program_Counter is
 
-	signal pc_internal_state : unsigned( 31 downto 0) :=  (others => '0'); -- signal for holding internal state of counter
+	signal pc_current : std_logic_vector( 31 downto 0); -- signal for holding internal state of counter
 
 begin
 
---------------
-----------
--- Process name: count_process
--- Purpose: Determines what should Program Counter do according to pc_command signal
--- Outcome: Change of pc_out or pc_internal_state signals
-----------
---------------
-		count_process : process(clk)
-		begin
-		
-			if rising_edge(clk) then
-			
-				if pc_internal_state = integer'high then -- If counter reaches max valuse It's reseted to 0
-						pc_internal_state <= (others => '0');
-				end if;
-			
-				
-			
-				case pc_command is 
-				
-				when "00" => pc_internal_state <=  (others => '0'); -- Program Counter is reseted
-				when  "01" => pc_internal_state <= pc_internal_state + 1;-- count
-				when "10" => pc_out <= std_logic_vector(pc_internal_state);  -- output pc_internal_state
-				when "11" => pc_internal_state <= unsigned(pc_in);  -- load Program Counter
-				when others => pc_internal_state <= ( others => 'Z'); -- when any other state it's high impedance
-				
-				end case;
-				
-			end if;
-		
-		end process;
-
+	pc_process : process(clk)
+	begin
+	
+	   pc_current <= pc_input;
+	   pc_output <= pc_current;
+	
+	end process;
 
 end Behavioral;
 
--- src/Register_Bank.vhd
+++ src/Register_Bank.vhd
@@ -13,16 +13,12 @@ use ieee.numeric_std.all;
 entity Register_Bank is
 
 	port(
-
-		clk : in std_logic; -- clock signal
-		reg_output_a : out std_logic_vector ( 31 downto 0); -- first output to ALU
-		reg_output_b : out std_logic_vector ( 31 downto 0); -- second output to ALU
-		reg_address_a : in std_logic_vector ( 4 downto 0); -- first input into regiter bank
-		reg_address_b : in std_logic_vector ( 4 downto 0); -- seoond input into register bank
-		reg_address_c : in std_logic_vector ( 4 downto 0); -- address for writing data to register bank
-		reg_input_data : in std_logic_vector ( 31 downto 0); -- data to write into regiser bank
-		reg_command : in std_logic_vector ( 1 downto 0)  -- command for regiter bank
-
+        red_reg1 : in std_logic_vector( 4 downto 0);
+        read_reg2 : in std_logic_vector ( 4 downto 0);
+        write_reg : in std_logic_vector ( 4 downto 0);
+        write_data : in std_logic_vector ( 31 downto 0);
+        read_data1 : out std_logic_vector ( 31 downto 0);
+        read_data2 : out std_logic_vector ( 31 downto 0)
 	);
 
 end Register_Bank;
@@ -36,33 +32,6 @@ architecture Behavioral of Register_Bank is
 
 begin
 
---------------
-----------
--- Process name: reg_process
--- Purpose: Accesses register bank to read data from register or write data to register
--- Outcome: Change of register values
-----------
---------------
-
-	reg_process : process (clk)
-	begin
-
-		if rising_edge(clk) then
-
-			case reg_command  is
-
-			when "01" => reg_bank(to_integer(unsigned(reg_address_c))) <= reg_input_data; -- load register
-			when "10" =>  																			-- read registers ( for alu operations )
-				reg_output_a <= reg_bank(to_integer(unsigned(reg_address_a)));
-				reg_output_b <= reg_bank(to_integer(unsigned(reg_address_b)));
-			when others => reg_output_a <= (others => 'Z'); --when no command is sent reg_ister output is on high impedance
-								reg_output_b <= (others => 'Z');
-
-			end case;
-
-		end if;
-
-	end process;
 
 
 end Behavioral;
