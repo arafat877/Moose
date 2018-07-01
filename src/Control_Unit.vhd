1f492a81d71ce3af7d816dac9ab24ad7735783de
lazardjurovic <lazar.lax4525@gmail.com>
2018-07-01 12:12:48 +0200

    created ports for Control Unit

--- src/Control_Unit.vhd
+++ src/Control_Unit.vhd
@@ -1,26 +1,18 @@
---------------
--- Module name: Control Unit
--- Control Unit is used to decode instructions and send parameters to ALU so instruction can be executed
--- Bit width : 32
---------------
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity Control_Unit is
 
 	port(
-		
-		clk : in std_logic; -- clock signal
-		instruction_word : in std_logic_vector ( 31 downto 0); -- size of instruction word is specified in RISC V datasheet
-		pc_command  : out std_logic_vector ( 1 downto 0); -- command for program counter
-		alu_command : out std_logic_vector ( 3 downto 0); -- output from ALU
-		alu_flags : in std_logic_vector ( 3 downto 0); -- flags from alu ( used for jump commands )
-		reg_command : out std_logic_vector ( 1 downto 0); -- command for register bank
-		reg_address : out std_logic_vector ( 4 downto 0) -- address for register in register bank
-	
-		
-		
+	   
+	   instruction : in std_logic_vector ( 6 downto 0);
+	   Branch : out std_logic;	
+	   MemeRead : out std_logic;
+	   MemtoReg : out std_logic;
+	   MemWrite : out std_logic;
+	   ALUSrc : out std_logic;
+	   RegWrite : out std_logic;
+	   ALUOp : out std_logic_vector ( 1 downto 0)
 	);
 
 end Control_Unit;
@@ -28,21 +20,7 @@ end Control_Unit;
 architecture Behavioral of Control_Unit is
 	
 begin
-
---------------
-----------
--- Process name: cu_process
--- Purpose: Fetching, Decoding and Executing instruction
--- Outcome: Instructoin is executed and result is stored in one of registers
-----------
---------------
-
-	cu_proces : process (clk)
-	begin
 	
-	
-	
-	end process;
 
 end Behavioral;
 
