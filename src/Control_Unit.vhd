a77094b181080b3011aeace71768828d2f151e9e
lazardjurovic <lazar.lax4525@gmail.com>
2018-07-03 16:11:36 +0200

    R type instruction done

--- src/Control_Unit.vhd
+++ src/Control_Unit.vhd
@@ -5,9 +5,9 @@ entity Control_Unit is
 
 	port(
 	   
-	   instruction : in std_logic_vector ( 6 downto 0);
+	   opcode : in std_logic_vector ( 6 downto 0);
 	   Branch : out std_logic;	
-	   MemeRead : out std_logic;
+	   MemRead : out std_logic;
 	   MemtoReg : out std_logic;
 	   MemWrite : out std_logic;
 	   ALUSrc : out std_logic;
@@ -20,7 +20,27 @@ end Control_Unit;
 architecture Behavioral of Control_Unit is
 	
 begin
-	
+
+    control_unit : process ( opcode )
+    begin
+    
+        case opcode is 
+        
+            when "0110011" =>  -- R type Instruction
+                Branch <= '0';
+                MemRead <= '0';
+                MemtoReg <= '0';
+                ALUOp <= "00";
+                MemWrite <= '0';
+                ALUSrc <= '0';
+                RegWrite <= '1';
+                
+                
+        end case;
+        
+      
+    
+    end process;
 
 end Behavioral;
 
