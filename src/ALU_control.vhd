44aab38ff463e4f970e0efe5ea101dff8e8557b9
lazardjurovic <lazar.lax4525@gmail.com>
2018-07-03 16:00:32 +0200

    added ALU_ control module

---
+++ src/ALU_control.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07/03/2018 03:33:59 PM
+-- Design Name: 
+-- Module Name: ALU_control - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ALU_control is
+--  Port ( );
+end ALU_control;
+
+architecture Behavioral of ALU_control is
+
+begin
+
+
+end Behavioral;
