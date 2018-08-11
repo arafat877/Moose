210cbdfe937fcb1b8a819a65abacf9d8c4ae57a7
Lazar Djurovic <lazar.lax4525@gmail.com>
2018-08-11 22:34:01 +0200

    Added clk and reset signals to ALU and ALU_control

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -10,12 +10,14 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity ALU is
 
-  port(       
+  port(  
+		  clk : in std_logic;
+		  reset : in std_logic;
         opcode : in std_logic_vector( 3 downto 0);
         input1 : in std_logic_vector ( 31 downto 0);
         input2 : in std_logic_vector ( 31 downto 0);
         output : out std_logic_vector ( 31 downto 0);
-        zero : out std_logic -- zero flag
+        zero : out std_logic := '0' -- zero flag
         );
   
 end ALU;
@@ -23,50 +25,57 @@ end ALU;
 architecture Behavioral of ALU is
 begin
 
-    alu_process : process(opcode)
+    alu_process : process(clk)
     begin
     
-        case opcode is
-            
-           when "0000"=> output <= (others => 'X');
-           when "0001"=>output <= std_logic_vector( unsigned(input1) + unsigned(input2));
-           when "0010"=> 
-          
-              output <=  std_logic_vector( unsigned(input1) - unsigned(input2));
-              if( unsigned(input1) - unsigned(input2) ) = 0 then
-                    zero <= '1';
-              end if; 
-          
-          
-           when "0011"=>output <= std_logic_vector(shift_left(unsigned(input1),to_integer(unsigned(input2)) ) );      
-           when "0100"=>
-                if signed(input1) < signed(input2) then
-                    output(31 downto 1) <= (others => '0');
-                    output(0) <= '1';
-                else 
-                    output <= (others => '0');
-               end if;
-             
-                         
-           when "0101"=>
-                if unsigned(input1) < unsigned(input2) then
-                    output(31 downto 1) <= (others => '0');
-                    output(0) <= '1';
-                else 
-                    output <= (others => '0');
-                end if; 
-                
-           when "0110"=> output <= input1 xor input2;
-           when "0111"=> output <= std_logic_vector(shift_right(unsigned(input1),to_integer(unsigned(input2)) ) );
-           when "1000"=> output <= std_logic_vector(shift_right(unsigned(input1),to_integer(signed(input2)) ) );
-           when "1001"=> output <= input1 or input2;
-           when "1010"=> output <= input1 and input2;
-           when others => output <= (others => 'X');            
-        end case;
-    
+	 if rising_edge(clk) then
+	 
+		if reset = '1' then
+			output <= (others => '0');
+			zero <= '0';
+		else
+	 
+			  case opcode is
+					
+				  when "0000"=> output <= (others => 'X');
+				  when "0001"=>output <= std_logic_vector( unsigned(input1) + unsigned(input2));
+				  when "0010"=> 
+				 
+					  output <=  std_logic_vector( unsigned(input1) - unsigned(input2));
+					  if( unsigned(input1) - unsigned(input2) ) = 0 then
+							  zero <= '1';
+					  end if; 
+				 
+				 
+				  when "0011"=>output <= std_logic_vector(shift_left(unsigned(input1),to_integer(unsigned(input2)) ) );      
+				  when "0100"=>
+						 if signed(input1) < signed(input2) then
+							  output(31 downto 1) <= (others => '0');
+							  output(0) <= '1';
+						 else 
+							  output <= (others => '0');
+						end if;
+					 
+									 
+				  when "0101"=>
+						 if unsigned(input1) < unsigned(input2) then
+							  output(31 downto 1) <= (others => '0');
+							  output(0) <= '1';
+						 else 
+							  output <= (others => '0');
+						 end if; 
+						 
+				  when "0110"=> output <= input1 xor input2;
+				  when "0111"=> output <= std_logic_vector(shift_right(unsigned(input1),to_integer(unsigned(input2)) ) );
+				  when "1000"=> output <= std_logic_vector(shift_right(unsigned(input1),to_integer(signed(input2)) ) );
+				  when "1001"=> output <= input1 or input2;
+				  when "1010"=> output <= input1 and input2;
+				  when others => output <= (others => 'X');            
+			  end case;
+		end if;
+	 end if;
+	 
     end process;
-    
-      
     
 
 end Behavioral;
--- src/ALU_control.vhd
+++ src/ALU_control.vhd
@@ -4,7 +4,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity ALU_control is
     
     port(
-        
+        clk : in std_logic;
+		  reset : in std_logic;
         instruction : in std_logic_vector ( 31 downto 0);
         ALUOp : in std_logic_vector ( 1 downto 0);
         alu_opcode : out std_logic_vector ( 3 downto 0 )  
@@ -19,49 +20,54 @@ architecture Behavioral of ALU_control is
 
 begin
 
-    funct3 <= instruction(14 downto 12);
-    funct7 <= instruction(31 downto 25);
-    
-    alu_control : process( funct3 , funct7, ALUOp)
+    alu_control : process(clk)
     begin
         
-        case ALUOp is 
-            
-            when "00" => alu_opcode <= "0001"; -- add is for load and store Instructions
-            when "01" => alu_opcode <= "0010"; -- sub is for branching
-            when "10" =>
-                
-                case funct3 is
-                    
-                    when "000"=>
-                        case funct7 is
-                            when "0000000" => alu_opcode <= "0001"; -- add
-                            when "0100000" => alu_opcode <= "0010"; -- sub
-									 when others => alu_opcode <= "ZZZZ";
-                        end case;
-                    when "001" => alu_opcode <= "0011";
-                    when "010" => alu_opcode <= "0100";
-                    when "011" => alu_opcode <= "0101";
-                    when "100" => alu_opcode <= "0110";
-                    when "101" =>
-                        case funct7 is
-                            when "0000000" => alu_opcode <= "0111";
-                            when "0100000" => alu_opcode <= "1000";
-									 when others => alu_opcode <= "ZZZZ";
-                        end case;
-                   when "110" => alu_opcode <= "1001";
-                   when "111" => alu_opcode <= "1011";
-						 when others => alu_opcode <= "ZZZZ";
-                        
-                    
-                end case;
-			when others => alu_opcode <= "ZZZZ";
-                
-                
-            
-        end case;
-        
-        
+	 if rising_edge(clk) then
+	 
+		if reset = '1' then
+			alu_opcode <= "0000";
+			funct3 <= "000";
+			funct7 <= "0000000";
+		else
+    		   funct3 <= instruction(14 downto 12);
+				funct7 <= instruction(31 downto 25);
+    
+			  case ALUOp is 
+					
+					when "00" => alu_opcode <= "0001"; -- add is for load and store Instructions
+					when "01" => alu_opcode <= "0010"; -- sub is for branching
+					when "10" =>
+						 
+						 case funct3 is
+							  
+							  when "000"=>
+									case funct7 is
+										 when "0000000" => alu_opcode <= "0001"; -- add
+										 when "0100000" => alu_opcode <= "0010"; -- sub
+										 when others => alu_opcode <= "ZZZZ";
+									end case;
+							  when "001" => alu_opcode <= "0011";
+							  when "010" => alu_opcode <= "0100";
+							  when "011" => alu_opcode <= "0101";
+							  when "100" => alu_opcode <= "0110";
+							  when "101" =>
+									case funct7 is
+										 when "0000000" => alu_opcode <= "0111";
+										 when "0100000" => alu_opcode <= "1000";
+										 when others => alu_opcode <= "ZZZZ";
+									end case;
+							 when "110" => alu_opcode <= "1001";
+							 when "111" => alu_opcode <= "1011";
+							 when others => alu_opcode <= "ZZZZ";
+									
+							  
+						 end case;
+				when others => alu_opcode <= "ZZZZ";
+						 			
+			  end case;
+        end if;
+		end if;   
     end process;
 
 end Behavioral;
